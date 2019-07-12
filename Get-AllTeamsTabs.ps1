a94d518ff78f85091af388c51088880a0e330c65
Ruben <ruben_rm@outlook.com>
2019-07-12 08:30:15 +0200

    Get-AllTeamsTabs.ps1 added

---
+++ Get-AllTeamsTabs.ps1
@@ -0,0 +1,52 @@
+﻿function Get-AllTeams {
+    param([string]$token)
+    
+    $url = "https://graph.microsoft.com/beta/groups?`$filter=resourceProvisioningOptions/Any(x:x eq 'Team')"
+    $response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Bearer $token"}
+    $response.value
+}
+
+function Get-TeamChannels {
+    param([string]$token, [System.Guid] $teamID)
+    
+    $url = "https://graph.microsoft.com/v1.0/teams/" + $teamID + "/channels"
+    $response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Bearer $token"}
+    $response.value
+}
+
+function Get-ChannelTabs {
+    param([string]$token, [System.Guid] $teamID, [string] $channelID)
+    
+    $url = "https://graph.microsoft.com/v1.0/teams/" + $teamID + "/channels/" + $channelID + "/tabs"
+    $response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Bearer $token"}
+    $response.value
+}
+
+$scopes = 'Group.Read.All'
+Connect-PnPOnline -Scopes $scopes
+
+$token = Get-PnPAccessToken
+
+$teams = Get-AllTeams $token
+
+$channels = @()
+
+foreach($team in $teams) {
+    $teamChannels = Get-TeamChannels $token $team.id
+
+    foreach($channel in $teamChannels) {
+        $channelToAdd = "" | Select-Object "TeamId","TeamName","ChannelId","ChannelName", "ChannelTabs"
+        $channelToAdd.TeamId = $team.id
+        $channelToAdd.TeamName = $team.displayName
+        $channelToAdd.ChannelId = $channel.id
+        $channelToAdd.ChannelName = $channel.displayName
+
+        $channelTabs = Get-ChannelTabs $token $team.id $channel.id
+
+        $channelToAdd.ChannelTabs = $channelTabs
+
+        $channels += $channelToAdd
+    }
+}
+
+$channels | Format-Table
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MSTeams-PS
+
+Repositorio donde iré almacenando scripts PowerShell que puedan resultar de utilidad para trabajar con Microsoft Teams
